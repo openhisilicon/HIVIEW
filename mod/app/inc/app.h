01e7e90528996b277ddbffc308f4f8d7820a87d8
HIVIEW <50411953+openhisilicon@users.noreply.github.com>
2020-04-07 17:02:20 +0800

    add app mod;

---
+++ mod/app/inc/app.h
@@ -0,0 +1,26 @@
+#ifndef __app_h__
+#define  __app_h__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "inc/msg.h"
+
+
+enum {
+    GSF_ID_APP_CFG  = 1,
+};
+
+enum {
+    GSF_APP_ERR = -1,
+};
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
